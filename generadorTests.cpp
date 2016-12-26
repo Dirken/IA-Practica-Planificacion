d85ceac11e73fd6c5d0594bfadac4009486c90c1
Dirken <ricard.meyerhofer@est.fib.upc.edu>
2016-12-26 18:41:42 +0100

    algo mejorada su escalabilidad, posteriormente la seguiremos mejorando

--- generadorTests.cpp
+++ generadorTests.cpp
@@ -13,32 +13,44 @@ struct test{
 	string tipo;
 };
 
+void printTests(){
+	cout << "Please indicate the number of tests that you would like to create:";
+}
+void printOptions(){
+	cout << "El test que generamos sobre qué extensión queremos que sea?" << endl;
+	cout << "0 - Básico" << endl;
+	cout << "1 - Extensión 1" << endl;
+	cout << "2 - Extensión 2" << endl;
+	cout << "3 - Extensión 3" << endl;
+	cout << "4 - Extensión 4" << endl;
+}
+
+string createNameDocument(int i){
+	stringstream sstm;
+	sstm << "problem" << i;
+	string result =sstm.str();
+	return result;
+}
+
 int main(){
 
 	/* initialize random seed: */
   	srand (time(NULL));
-
-	int number_tests;
-	cout << "Please indicate the number of tests that you would like to create:";
+  	printTests();
+	int number_tests, version;
 	cin >> number_tests;
+	printOptions();
+	cin >> version;
 
+	
 	for (int i = 0; i < number_tests; ++i){
-		
-		stringstream sstm;
-		sstm << "problem" << i;
-		string result =sstm.str();
-		//Create document
-		ofstream outputFile(result.c_str());
-
+		createNameDocument(i);
+		ofstream outputFile(createNameDocument(i).c_str());	
 		outputFile << "(define (problem visual-content-problem)" << endl;
 		outputFile << "(:domain visual-content)" << endl;
 		outputFile << "(:objects" << endl;
-		
-		
-
 		/* generate secret number between 1 and 100 */
   		int random_number = rand() % 100 +1;
-  		
   		vector<test> v(random_number);
 		//Objects
 
